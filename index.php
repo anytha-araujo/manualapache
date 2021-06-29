968a5758dbf05d86675ea5a6e3015affee39b629
anytha-araujo <60233995+anytha-araujo@users.noreply.github.com>
2021-06-29 14:40:50 -0300

    Adicionando index.php
    Adicionando index.php

---
+++ index.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <!--====== Title ======-->
+    <title>Hands-on SPREV</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/logo-2.svg" width="130" height="125">
+
+    <!--====== Magnific Popup CSS ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <!--====== Slick CSS ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+
+    <!--====== Line Icons CSS ======-->
+    <link rel="stylesheet" href="assets/css/LineIcons.css">
+
+    <!--====== Bootstrap CSS ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!--====== Default CSS ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+
+    <!--====== Style CSS ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+    <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+    <!--====== PRELOADER PART START ======-->
+
+    <div class="preloader">
+        <div class="loader">
+            <div class="ytp-spinner">
+                <div class="ytp-spinner-container">
+                    <div class="ytp-spinner-rotator">
+                        <div class="ytp-spinner-left">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                        <div class="ytp-spinner-right">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== PRELOADER PART ENDS ======-->
+
+    <!--====== NAVBAR TWO PART START ======-->
+
+    <section class="navbar-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <a class="navbar-brand" href="#">
+                            <img src="assets/images/logo.svg" alt="Logo" width="130" height="125">
+                        </a>
+                        <div class="collapse navbar-collapse sub-menu-bar" id="navbarTwo">
+                            <ul class="navbar-nav m-auto">
+                                <li class="nav-item"><a class="page-scroll" href="#team">MARKETPLACE</a></li>
+                                <li class="nav-item"><a class="page-scroll" href="#contact">PORTAL DA DATAPREV</a></li>
+                            </ul>
+                    </nav> <!-- navbar -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== NAVBAR TWO PART ENDS ======-->
+
+    <!--====== SLIDER PART START ======-->
+
+    <section id="home" class="slider_area">
+        <div id="carouselThree" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="slider-content" align="center">
+                                    <img src="assets/images/GovCloud.png" width="225" height="105">
+                                    <br>
+                                    <br>
+                                    <p class="title"><h2>Etapa final do Hands-on SPREV!<h2></p>
+                                    <p class="text"><h4>Role a página e confira o resultado do balanceamento configurado<h4></p>
+                                    <br>
+                                    <br>
+                                    <img src="assets/images/adown-wh.png" class="bounce">
+                                </div>
+                            </div>
+                        </div> <!-- row -->
+                    </div> <!-- container -->
+                    <div class="slider-image-box d-none d-lg-flex align-items-end">
+                        <div class="slider-image">
+                            <img src="assets/images/slider/1.png" alt="Hero">
+                        </div> <!-- slider-imgae -->
+                    </div> <!-- slider-imgae box -->
+                </div> <!-- carousel-item -->
+        </div>
+    </section>
+
+    <!--====== SLIDER PART ENDS ======-->
+
+    <!--====== PRINICNG START ======-->
+
+    <section id="pricing" class="pricing-area">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 col-md-10">
+                    <div class="section-title text-center pb-25">
+                      <div class="center">
+                          <div class="balao2">
+                            <br>
+                            <?php
+                                $nome = $_SERVER['SERVER_NAME'];
+                                $ip = $_SERVER['SERVER_ADDR'];
+                                echo "<b><center><font color='#000000'>O Servidor </font></b>".$ip."<b><font color='#000000'> está prestando este atendimento! Pressione <b>F5</b> para mudar de servidor e testar o balanceamento.</font></center></b>";
+                             ?>
+                         </div> <!-- section title -->
+                       </div>
+                </div>
+            </div> <!-- row -->
+                <div class="col-lg-10 col-md-8 col-sm-15">
+                            <img src="assets/images/robot3.png" width="400" height="420" class="center">
+                            <br>
+                        </div>
+                    </div> <!-- pricing style one -->
+                    <center><p class="text"><h3><font color='#000000'>Agradecemos sua participação!</font><h3></p></center>
+                </div>
+            </div> <!-- container -->
+    </section>
+
+    <!--====== PRINICNG ENDS ======-->
+
+    <!--====== FOOTER PART START ======-->
+
+    <section class="footer-area footer-dark">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="footer-logo text-center">
+                        <a class="mt-30" href="index.html"><img src="assets/images/LogoDTP.png" alt="Logo" width="140" height="100"></a>
+                    </div> <!-- footer logo -->
+                    <ul class="social text-center mt-60">
+                        <li><a href="http://www.facebook.com/dataprevtecnologia"><i class="lni lni-facebook-filled"></i></a></li>
+                        <li><a href="http://www.twitter.com/dataprev"><i class="lni lni-twitter-original"></i></a></li>
+                        <li><a href="https://www.instagram.com/dataprev/"><i class="lni lni-instagram-original"></i></a></li>
+                        <li><a href="https://www.instagram.com/dataprev"><i class="lni lni-linkedin-original"></i></a></li>
+                    </ul> <!-- social -->
+                    <div class="copyright text-center mt-35">
+                        <p class="text"><h4>Pós-Venda GovCloud</h4></p>
+                    </div> <!--  copyright -->
+                    <div class="footer-support text-center">
+                        <span class="mail">dipv@dataprev.gov.br</span>
+                    </div>
+
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== FOOTER PART ENDS ======-->
+
+    <!--====== BACK TOP TOP PART START ======-->
+
+    <a href="#" class="back-to-top"><i class="lni lni-chevron-up"></i></a>
+
+    <!--====== BACK TOP TOP PART ENDS ======-->
+
+    <!--====== PART START ======-->
+
+<!--
+    <section class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-">
+
+                </div>
+            </div>
+        </div>
+    </section>
+-->
+    <!--====== PART ENDS ======-->
+
+
+
+
+    <!--====== Jquery js ======-->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <!--====== Ajax Contact js ======-->
+    <script src="assets/js/ajax-contact.js"></script>
+
+    <!--====== Isotope js ======-->
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+
+    <!--====== Scrolling Nav js ======-->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
